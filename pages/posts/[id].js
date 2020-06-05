db5574c1ce43f766ecad05779df8adb0fe53a3b9
Juaquin Gomez <juaquin.gomez619@gmail.com>
2020-06-05 18:11:35 -0400

    spelling in first

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,7 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import React from "react";
 
 export default function Post({ postData }) {
   return (
